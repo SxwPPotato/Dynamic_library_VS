e4b0b76744c2b5207a95f51f8d009ee70e5516a4
Vitalya <vitalyabes.vb@gmail.com>
2023-11-27 11:18:24 +0300

    Second comit

---
+++ Dynamic_project_dll/Leaver.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#ifdef DYNAMIC_PROJECT.DLL_EXPORTS
+#define LEAVER_API __declspec(dllexport)
+#else
+#define LEAVER_API __declspec(dllimport)
+#endif
+
+
+	class Leaver {
+	private:
+		std::string name;
+	public:
+		LEAVER_API Leaver(std::string name);
+		LEAVER_API void leave();
+
+	};
